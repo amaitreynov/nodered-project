52002025c2a7bff7a6bb1c38a3526659136ef572
lnans <nans.laupretre@gmail.com>
2017-03-13 22:50:33 +0100

    POST : SetAlarm OK

--- routes/doorSensor.js
+++ routes/doorSensor.js
@@ -4,10 +4,16 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	console.log("on est dans l'index du door sensor");
+    res.render('index', { title: 'Wake up IoT' , subTitle: "Home door sensor"});
+});
+
 /* POST set door sensor alarm time according to what's been posted via /smartphone/setAlarm page. */
 router.post('/setAlarm', function(req, res, next) {
-		//todo link to nodered
-		res.render('index', { title: 'Wake up IoT' , subTitle: "Door sensor - Set alarm"});
+		console.log("On est dans le set alarm du door sensor");
+		res.redirect('/doorsensor/');
 });
 
 /* POST snooze. */
--- routes/smartphone.js
+++ routes/smartphone.js
@@ -3,29 +3,37 @@
  */
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-		res.render('index', { title: 'Wake up IoT' , subTitle: "Home"});
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Wake up IoT', subTitle: "Home"});
 });
 
 /* POST set smartphone alarm time page. */
-router.post('/setAlarm', function(req, res, next) {
-		//todo link to nodered
-		res.render('smartphone', { title: 'Wake up IoT' , subTitle: "Smartphone - Set alarm"});
+router.post('/setAlarm', function (req, res, next) {
+    request.post({url:'http://127.0.0.1:1880/setAlarm', form: {key:'value'}}, function(err,httpResponse,body){
+        if(err)
+            console.log(err);
+        else {
+            // console.log(httpResponse);
+            // console.log(JSON.stringify(body));
+            res.render('smartphone', { title: 'Wake up IoT' , subTitle: "Smartphone - Alarm Set successfully"});
+        }
+    });
 });
 
 /* GET set smartphone alarm time page. */
-router.get('/setAlarm', function(req, res, next) {
-		//todo link to nodered
-		res.render('smartphone', { title: 'Wake up IoT' , subTitle: "Smartphone - Set alarm"});
+router.get('/setAlarm', function (req, res, next) {
+    //todo link to nodered
+    res.render('smartphone', {title: 'Wake up IoT', subTitle: "Smartphone - Set alarm"});
 });
 
 /* POST set smartphone alarm time page. */
-router.post('/snooze', function(req, res, next) {
-		//todo link to nodered
-		res.render('smartphone', { title: 'Wake up IoT' , subTitle: "Smartphone - Snooze"});
+router.post('/snooze', function (req, res, next) {
+    //todo link to nodered
+    res.render('smartphone', {title: 'Wake up IoT', subTitle: "Smartphone - Snooze"});
 });
 
 module.exports = router;
